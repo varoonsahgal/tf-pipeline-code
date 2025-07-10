942572c0c4dd45af008b5de8fac0d59d694b7190
varoonsahgal <varoon.sahgal@gmail.com>
2025-07-10 12:21:05 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,3 +60,16 @@ resource "azurerm_linux_virtual_machine" "main" {
     version   = "latest"
   }
 }
+
+resource "azurerm_storage_account" "test" {
+  name                     = "mytfstorageacctvs17"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "test"
+  }
+}
+
